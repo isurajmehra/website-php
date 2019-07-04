c609d933065fb81c13adffb4217d19539209f14e
pranaysharma <pranaysharma995@gmail.com>
2019-07-04 12:28:46 +0530

    Remove portfolio html and update web-app-development and partnership pages

---
+++ partnership-page/referal-partner.php
@@ -0,0 +1,413 @@
+<!doctype html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=shift_jis">
+<title>Acropolis Infotech</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" type="image/png" href="../assets/img/fevicon/fevicon.png"/>
+    <meta property="og:title" content="Enterprise Customer Software Development Company | Mobile App | Web App | IT Services">
+<meta property="og:description" content="Enterprise Customer Software Development Company | Mobile App | Web App | IT Services">
+<meta property="og:image" content="https://www.acropolisinfotech.com/assets/img/acropolis-facebook.png">
+<meta property="og:image:alt" content="Acropolis Infotech">
+<meta property="og:url" content="https://www.acropolisinfotech.com/">
+<meta property="og:type" content="website" />
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"  media="none" onload="if(media!='all')media='all'">
+<link href="../assets/css/web-development.css" rel="stylesheet" />
+<link href="../assets/css/mobile-style.css" rel="stylesheet" media="none" onload="if(media!='all')media='all'" />
+<link rel="stylesheet" href="assets/owl-carousel/dist/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/owl-carousel/dist/assets/owl.theme.default.min.css">
+<style type="text/css">
+            .wearable-app-development-container .col-md-3 img,.wearable-app-solution-container .col-md-3 img{
+        width:auto;
+        max-width:100%;
+        height:685px;
+    }
+       .wearable-solution-box-container {
+    position: absolute;
+    top: 30%;
+    right: 30%;
+    width: 300px;
+    height: 300px;
+    clear: both;
+    background-color: #fff;
+    z-index: 3;
+    box-shadow: 1px 1px 10px #333;
+}
+ .wearable-box:hover{
+            background-color:#f0f0f0;
+        }
+    .wearable-box.active{
+        background-color:#f0f0f0;
+        color:#0480a2;
+    }
+     .wearable-box.active  svg path{
+         fill:#fff;
+     }
+    .wearable-explore-button{
+            background-color:#333;
+    text-decoration:none;
+    padding:20px 30px;
+    color:#fff;
+    text-decoration:none;
+    border:2px solid #333
+    }
+    .wearable-box{
+        width:50%;
+        height:50%;
+        float:left;
+    }
+        .healthcare-component-image{
+            background-image:url(../assets/img/industries/healthcare/background.jpg);
+            background-attachment:scroll;
+            background-size:cover;
+            background-position:center;
+            background-repeat:no-repeat;
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            z-index:1;
+        }
+        .healthcare-components{
+            z-index:3;
+        }
+    .healthcare-component-title{
+        position:relative;
+        border-bottom:1px solid blue;
+        padding:30px 0px;
+    }
+    .healthcare-component-title::after{
+        content:"";
+        position:absolute;
+        bottom:0;
+        left:0;
+        width:30%;
+        height:3px;
+        background-color:blue;
+        z-index:5;
+        
+    }
+        .healthcare-component-description{
+            color:gray;
+            line-height:1.5;
+        }
+        .industries-provider li,.industries-software li{
+            padding:30px 30px;
+        }
+        .industry-overview-container{
+            min-height:800px;
+        }
+        .industries-software{
+            position:absolute;
+            top:-100px;
+            left:0;
+            z-index:3;
+        }
+        .industries-provider{
+            position:absolute;
+            top:0;
+            right:0;
+            z-index:3;
+        }
+        .hospitality-it-solution-box{
+            transition:all 0.3s linear;
+            -webkit-transition:all 0.3 linear;
+                border:1px solid transparent;
+                box-shadow:1px 1px 20px #999;
+                border-radius:5px;
+                
+        }
+        
+        .hospitality-it-solution-box h3{
+            border-left:5px solid blue;
+        }
+        
+        .hospitality-it-solution-box{
+            position:relative;
+        }
+        .hospitality-it-solution-box:hover{
+            border:1px solid blue;
+            transform:scale(1.1);
+        }
+        
+        .hospitality-it-solution-box:hover{
+            color:blue;
+        }
+        .referal-partner{
+            width:100%;
+            max-width:100%;
+        }
+        .referal-description{
+            position:relative;
+            padding:0px 0px;
+        }
+        .referal-partner-description{
+            position:absolute;
+            bottom:40px;
+            left:0px;
+            width:100%;
+            z-index:3;
+            padding:0px 30px;
+        }
+        .referal-partner-description h3{
+            font-size:25px;
+            line-height:35px;
+            font-style:italic;
+            font-family:clanpro-bold;
+        }
+                .black-overlay{
+            background: linear-gradient(rgba(0, 0, 0, 0.02), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
+            position:absolute;
+            bottom:0;
+            left:0;
+            width:100%;
+            height:30%;
+            z-index:2;
+            border-bottom-left-radius:5px;
+            border-bottom-right-radius:5px;
+        }
+        
+.referral-service-list li{
+    font-size:18px;
+    font-family:clanpro;
+    color:#444;
+    margin:10px 0px;
+}
+#referral-partner img{
+    border-radius:5px;
+}
+.scrolling-list-container{
+    top:-36px;
+}
+
+</style>
+</head>
+<body>
+    <!-- opening of footer-fixed-component which include chat and contact detail -->
+    <div class="scrolling-top-button">
+       <img src="../assets/img/upload.png" alt="Arrow-top" />
+    </div>
+    <div class="scrolling-list-component">
+        <div class="scrolling-button-container">
+                <div class="scrolling-button"><span class="dot-circle one"></span>
+                <span class="dot-circle two"></span>
+                <span class="dot-circle three"></span></div>
+                    <div class="scrolling-list-container">
+        <ul class="scrolling-list relative">
+            <div class="caret-right">
+                <img src="../assets/img/caret-right.png" alt="caret-right" />
+            </div>
+            
+        </ul>
+    </div>
+    </div>
+
+
+</div>
+     	  
+    <!-- opening of footer-fixed-component which include chat and contact detail -->
+    <?php include '../template/side-widget.php'; ?>
+    <!-- closing of live-chat -->
+    <!-- opening of cookies banner -->
+    <!-- closing of cookies banner container -->
+    <div  class="container-fluid">
+<!-- opening of top nav desktop-version -->
+<?php include '../template/header.php'; ?>
+<!-- closing of top-nav  desktop-version-->
+<!-- opening of top-nav mobile version -->
+<?php include '../template/header-mobile.php'; ?>
+<!-- closing of top-nav mobile-version -->
+<div class="row hero-banner">
+    <picture>
+            <source  srcset="../assets/img/banner/webp/referral-partner.webp" alt="webp">
+            <source  srcset="../assets/img/banner/jp2/referral-partner.jp2" alt="jp2">
+            <img src="../assets/img/banner/referral-partner.jpg" alt="jpg">
+        </picture>
+    <div class="hero-banner-text-container">
+        <div class="hero-banner-title-box pd-30-60">
+            <h3 class="color-white clanpro-bold fz-60 pd-0-30">Referral Partner</h3>
+            
+        </div>
+          <div class="hero-banner-description-box pd-30-30 hero-banner-description-line">
+            <p class="color-white clanpro fz-30 pd-60-0">
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+    </div>
+   <div class="hero-banner-box">
+        <div class="hero-banner-box-pagelink-container">
+        <ul class="list list-inline hero-banner-box-list">
+           
+            </ul>
+            </div>
+            <div class="hero-banner-box-button-container">
+                <a href="#" class="hero-banner-box-arrow-button left "><i class="fa fa-angle-left"></i></a>
+                <a class="hero-banner-box-arrow-button right active" href="../partnership-page/affiliate-partner.html"><i class="fa fa-angle-right"></i></a>
+        </div>
+    </div>
+
+
+</div>
+<div class="row service-overview pd-60-0 relative page-section scrolling-section" id="overview">
+     <div class="background-title">
+        <h3 class="web-services">Referral</h3>
+        
+    </div>
+    <div class="col-md-12">
+        <h1 class="service-page-title pd-0-60 align-center clanpro-bold fz-48">Acropolis Referral Partner</h1>
+    </div>
+    <div class="col-md-6 service-overview-stats pd-60">
+        <p class="pd-0-60 color-green clanpro fz-21">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        
+    </div>
+    <div class="col-md-6 service-overview-description pd-60">
+        <p class="pd-0-60 clanpro fz-21">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        
+    </div>
+    <div class="col-md-12 pd-30-0">
+        <p class="pd-0-60 clanpro fz-21">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        
+    </div>
+    
+</div>
+<!--opening of referal partner profile-->
+<div class="row pd-0-0 bg-white pd-30-0 page-section scrolling-section" id="referral-partner">
+    <h1 class="section-title color-green align-center clanpro-bold fz-48">Referral Partner Profile</h1>
+    <div class="col-md-12 pd-60-0 ">
+        <div class="col-md-4">
+            <div class="col-md-12 pd-30-0 mgb-35 bg-white referal-description">
+                <img class="mg-0-0 referal-partner" src="../assets/img/partnership/existing-client.jpg" alt="Existing Client">
+                <div class="referal-partner-description">
+                    <h3 class="color-white">Online <br /> Booking</h3>
+                </div>
+                <div class="black-overlay">
+                    
+                </div>
+            </div>
+        </div>
+    
+        <div class="col-md-4">
+            <div class="col-md-12 pd-30-0 mgb-35 bg-white referal-description">
+                <img class="mg-0-0 referal-partner" src="../assets/img/partnership/technology-consultant.png" alt="Technology Consultant">
+                <div class="referal-partner-description">
+                    <h3 class="color-white">Online <br /> Booking</h3>
+                </div>
+                <div class="black-overlay">
+                    
+                </div>
+            </div>
+        </div>
+    
+    
+        <div class="col-md-4">
+            <div class="col-md-12 pd-30-0 mgb-35 bg-white referal-description">
+                <img class="mg-0-0 referal-partner" src="../assets/img/partnership/business-advisors.jpg" alt="Business Advisor">
+                <div class="referal-partner-description">
+                    <h3 class="color-white">Online <br /> Booking</h3>
+                </div>
+                <div class="black-overlay">
+                    
+                </div>
+            </div>
+        </div>
+       
+    </div>
+</div>
+<!--closing of referal partner profile-->
+<div class="row scrolling-section flex page-section referal-bottom"  id="what-you-get">
+    <div class="col-md-4 col-sm-12 col-xs-12 flex order-1 pd-60-60 service-sample-link bg-green pd-0-0" >
+        <h3 class="referal-heading color-white clanpro-bold bg-green  pd-60-60 fz-45">Referral Partner Program Benefits</h3>
+        
+    </div>
+    <div class="col-md-4 service-sample-image desktop-version flex order-2 pd-0-0">
+        <img src="../assets/img/banner/web-develoment.jpg">
+        
+    </div>
+    <div class="col-md-4 service-sample-description pd-100-90 flex order-3 bg-gray" >
+        <ul class="list referral-service-list mgt-30">
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+             <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+               <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+        </ul>
+       <div class="explore-more-button-container mgt-15">
+                    <a href="portfolio" class="explore-more-button clanpro color-white">
+            <div class="button-text">
+             <div class="lets-explore align-right">Let's Explore</div>
+        <div class="more clanpro-bold uppercase align-right">More</div>
+        </div>
+        <div class="button-icon"><span class="arrow-icon"><i class="fa fa-angle-right"></i></span>
+        </div>
+        </a>
+        </div>
+        
+    </div>
+</div>
+
+<!-- opening of start project section -->
+<?php include '../template/footer-project.php'; ?>
+<!-- opening of footer section -->
+<?php include '../template/footer.php'; ?>
+    <!-- closing of footer section -->
+    </div>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5bb5ad91b033e9743d0235ca/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<script src="assets/owl-carousel/dist/owl.carousel.min.js"></script>
+<script type="text/javascript" src="../assets/script/script.js">
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+       $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+        
+        
+        var service_title=["PHP Develoment","Open Source Development","ROR Development","HTML5 Develooment","Ajax Development","UI/UX Development","NodeJs Development","ExpressJS Development"];
+        var service_description=["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."];
+        $(".service-sample-list li").on("mouseover",function()
+        {
+            $(".service-sample-list li").removeClass("active");
+            $(this).addClass("active");
+            var index=$(this).index();
+            $(".service-title").text(service_title[index]);
+            $(".service-description").text(service_description[index]);
+        })
+        var scroll_container=$(".domain-expertise-list");
+        
+    })
+</script>
+
+</body>
+</html>
